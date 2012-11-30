7373608b411a46735c37b3b34ae5a9d0b4c17904
Aaron Cruz <aaron@aaroncruz.com>
2012-11-30 14:31:08 +0100

    Moved to css in jqtouch (instead of scss)

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,8 +1,11 @@
-//= require ./vendor/jquery.min
+//= require ./vendor/zepto.min
 //= require ./vendor/underscore-min
 //= require ./vendor/backbone-min
+//= require ./vendor/jqtouch.min
 //= require_self
 //= require ./backbone/app
+
+$.jQTouch();
 
 window.App = window.App || {};
 
