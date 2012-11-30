cfd6410f12df643c4b34154a9321c83d30b12359
Aaron Cruz <aaron@aaroncruz.com>
2012-11-30 17:30:26 +0100

    Lookin good

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,28 +1,10 @@
-//= require ./vendor/jquery.min
+//= require ./vendor/zepto.min
 //= require ./vendor/underscore-min
 //= require ./vendor/backbone-min
+//= require ./vendor/jqtouch.min
 //= require_self
-//= require ./backbone/app
+// require ./backbone/app
 
-window.App = window.App || {};
-
-window.App.init = function () {
-  jQuery(function () {
-    window.mainRouter = new App.MainRouter;
-    Backbone.history.start();
-  });
-};
-
-window.App.PhoneGap = function () {}
-
-App.PhoneGap.prototype.init = function () {
-  this.bindEvents();
-}
-
-App.PhoneGap.prototype.bindEvents = function () {
-  document.addEventListener('deviceready', this.onDeviceReady, false);
-}
-
-App.PhoneGap.prototype.onDeviceReady = function () {
-  App.init();
-}
+var jQT = $.jQTouch({
+    statusBar: 'black'
+});
